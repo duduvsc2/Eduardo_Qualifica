fa36fe79ac344ff57d2377658788287066bba42a
Eduardo dos santos Vasconcellos <duduvsc2@gmail.com>
2026-06-18 20:05:07 -0300

    Add files via upload
    Primeiros arquivos adicionados.

---
+++ script.js
@@ -0,0 +1,9 @@
+const botao = document.getElementById("meuBotao");
+
+botao.addEventListener('click', function() {
+
+    document.body.style.backgroundColor
+
+});
+
+alert("A página não recarregou 😉");
